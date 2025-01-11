a4d0593ddf2f571c9cf2e6cd57d44fab6f239e39
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2025-01-11 10:56:50 -0300

    adicionado nodemon para reload automatico do projeto em ambiente dev

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.send("Hello World Novo");
 });
 
 app.get("/user/:nome/sobrenome/:sobrenome", function(req, res) {
